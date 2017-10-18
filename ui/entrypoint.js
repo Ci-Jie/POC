79b411ad912a9735581ebcae641a2b627cff0e0a
cijie.l <cjlee8110@gmail.com>
2017-10-18 22:58:51 +0800

    Add entrypoint.sh in ui

--- ui/entrypoint.js
+++ ui/entrypoint.js
@@ -5,6 +5,7 @@ var APIHost = process.env.API_HOST;
 shell.sed('-i', '<API_HOST>', APIHost, 'config/prod.env.js');
 shell.exec('npm run build');
 shell.cd('dist/');
-shell.cp('', 'index.html', '/usr/local/apache2/htdocs/');
+shell.cp('index.html', '/usr/local/apache2/htdocs/');
 shell.cp('-R', 'static', '/usr/local/apache2/htdocs/');
-shell.exec('httpd-foreground')
+shell.cd('..');
+shell.exec('./entrypoint.sh');
