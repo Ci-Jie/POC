77a0f99dd48700fcf3e9ff2b8cff483b1607fe99
cijie.l <cjlee8110@gmail.com>
2017-10-18 17:20:00 +0800

    Add dockerfile and entrypoint in ui

---
+++ ui/entrypoint.js
@@ -0,0 +1,10 @@
+var shell = require('shelljs');
+
+var APIHost = process.env.API_HOST;
+
+shell.sed('-i', '<API_HOST>', 'http://' + APIHost, 'config/prod.env.js');
+shell.exec('npm run build');
+shell.cd('dist/');
+shell.cp('-R', 'index.html', '/usr/local/apache2/htdocs/');
+shell.cp('-R', 'static', '/usr/local/apache2/htdocs/');
+shell.exec('httpd-foreground');
