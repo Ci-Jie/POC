94aa22920ddd6d71daf523b3483c955f6ff2b3ce
cijie.l <cjlee8110@gmail.com>
2017-10-18 15:32:27 +0800

    Fix some bugs in app

--- api/entrypoint.js
+++ api/entrypoint.js
@@ -5,11 +5,10 @@ var address = process.env.ADDRESS;
 var newIPs = '';
 
 for (var index = 0; index < nodeIPs.split(",").length; index++) {
-  newIPs += 'http://' + nodeIPs.split(",")[index];
+  newIPs += '\'http://' + nodeIPs.split(",")[index] + '\'';
   if (index != nodeIPs.split(",").length - 1) newIPs += ',';
 }
 
-shell.cd('/POC/api')
-shell.sed('-i', '<NODE_IPS>', '[\'' + newIPs + '\']', 'routes/index.js');
+shell.sed('-i', '<NODE_IPS>', '[' + newIPs + ']', 'routes/index.js');
 shell.sed('-i', '<ADDRESS>', '\'' + address + '\'', 'routes/index.js');
-shell.exec('npm install')
+shell.exec('npm start')
